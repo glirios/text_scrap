66b900739d26f3c6655a42fee4edc51efc0ae019
Giovanni Lirios <lirios311@gmail.com>
2022-06-27 08:48:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import pdfplumber
+import string
 
 def main():
-	dict = {}
+	counter = {}
 
 	pdf = pdfplumber.open("Resume Giovanni Lirios.pdf")
 	page = pdf.pages[0]
 
 	# print(type(first_page.extract_text()))
 	for word in page.extract_text().split(" "):
-		print(word)
+		text = word.strip(string.punctuation + "\n")
+		if text != '':
+			if text in counter:
+				counter[text] += 1
+			else:
+				counter[text] = 1
+
+	print(counter)
 
 if __name__ == "__main__":
 	main()
